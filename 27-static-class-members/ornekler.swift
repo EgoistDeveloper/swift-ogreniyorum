8c6e65feb8a17b54c09b6dcf62d1276a40ed84bf
Arzu Can <60717140+EgoistDeveloper@users.noreply.github.com>
2025-08-02 20:04:17 +0300

    Update ornekler.swift

--- 27-static-class-members/ornekler.swift
+++ 27-static-class-members/ornekler.swift
@@ -1,16 +1,32 @@
-// Örnek - Statik Üye
-class MyClass {
-    static var staticProperty = "Static Property"
-    var instanceProperty = "Instance Property"
-
-    static func staticMethod() {
-        print("Called static method")
+// 1. Static property ve method
+struct Ayarlar {
+    static var tema: String = "Açık"
+    static func temaDegistir(_ yeniTema: String) {
+        tema = yeniTema
     }
 }
+Ayarlar.temaDegistir("Koyu")
+print(Ayarlar.tema) // Koyu
 
-// Kullanım
-print(MyClass.staticProperty) // Static Property
-MyClass.staticMethod() // Called static method
+// 2. Static property ile sayaç
+struct Kullanici {
+    static var toplam = 0
+    var ad: String
+    init(ad: String) {
+        self.ad = ad
+        Kullanici.toplam += 1
+    }
+}
+let k1 = Kullanici(ad: "Ada")
+let k2 = Kullanici(ad: "Turing")
+print(Kullanici.toplam) // 2
 
-let myInstance = MyClass()
-print(myInstance.instanceProperty) // Instance Property
+// 3. Class method override
+class Arac {
+    class func tur() -> String { "Genel Araç" }
+}
+class Bisiklet: Arac {
+    override class func tur() -> String { "Bisiklet" }
+}
+print(Arac.tur())     // Genel Araç
+print(Bisiklet.tur()) // Bisiklet
